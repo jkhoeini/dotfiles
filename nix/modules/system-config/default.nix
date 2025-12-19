0dae934d69ec290288a15a54c57b9661bea2837b
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2025-12-19 13:20:14 +0100

    feat: moved settings to nix

--- nix/modules/system-config/default.nix
+++ nix/modules/system-config/default.nix
@@ -21,6 +21,13 @@
 
   networking.computerName = "${username}'s work macbook pro";
   networking.hostName = "${username}-work-macbook-pro";
+  networking.knownNetworkServices = [
+    "USB 10/100/1000 LAN"
+    "Thunderbolt Ethernet Slot 0"
+    "Wi-Fi"
+    "Thunderbolt Bridge"
+    "iPhone USB"
+  ];
   networking.dns = [ "127.0.0.1" ];
 
   nix.settings.experimental-features = [
@@ -31,13 +38,31 @@
   security.pam.services.sudo_local.touchIdAuth = true;
   security.pam.services.sudo_local.watchIdAuth = true;
   system.defaults.NSGlobalDomain._HIHideMenuBar = false; # don't autohide menu bar
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false; # Enable repeat keys
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = false;
+  system.defaults.NSGlobalDomain.NSWindowResizeTime = 0.001;
+  system.defaults.controlcenter.BatteryShowPercentage = true;
+  system.defaults.controlcenter.NowPlaying = true;
   system.defaults.dock.autohide = true;
   system.defaults.dock.expose-animation-duration = 1.0e-3;
   system.defaults.dock.launchanim = false;
   system.defaults.dock.mru-spaces = false;
   system.defaults.finder.ShowPathbar = true;
+  system.defaults.finder._FXSortFoldersFirst = true;
+  system.defaults.finder._FXSortFoldersFirstOnDesktop = true;
+  system.defaults.menuExtraClock.Show24Hour = true;
+  system.defaults.trackpad.Clicking = true;
+  system.defaults.trackpad.DragLock = true;
+  system.defaults.trackpad.TrackpadFourFingerPinchGesture = 2;
+  system.defaults.trackpad.TrackpadFourFingerVertSwipeGesture = 2;
+  system.defaults.trackpad.TrackpadPinch = true;
+  system.defaults.trackpad.TrackpadRightClick = true;
+  system.defaults.trackpad.TrackpadRotate = true;
   system.defaults.trackpad.TrackpadThreeFingerDrag = true;
+  system.defaults.universalaccess.mouseDriverCursorSize = 2.0;
   system.defaults.universalaccess.reduceMotion = true;
+  system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
 
   # Set Git commit hash for darwin-version.
--- setup.rb
+++ setup.rb
@@ -43,63 +43,6 @@ unless File.exist?(File.expand_path('~/.intellimacs'))
   system 'git clone https://github.com/MarcoIeni/intellimacs ~/.intellimacs'
 end
 
-MACOS_DEFAULTS = {
-  # Enable repeat keys
-  ApplePressAndHoldEnabled: '-bool false',
-
-  # opening and closing windows and popovers
-  NSAutomaticWindowAnimationsEnabled: '-bool false',
-
-  # smooth scrolling
-  NSScrollAnimationEnabled: '-bool false',
-
-  # showing and hiding sheets, resizing preference windows, zooming windows
-  # float 0 doesn't work
-  NSWindowResizeTime: '-float 0.001',
-
-  # opening and closing Quick Look windows
-  QLPanelAnimationDuration: '-float 0',
-
-  # rubberband scrolling (doesn't affect web views)
-  NSScrollViewRubberbanding: '-bool false',
-
-  # resizing windows before and after showing the version browser
-  # also disabled by NSWindowResizeTime -float 0.001
-  NSDocumentRevisionsWindowTransformAnimation: '-bool false',
-
-  # showing a toolbar or menu bar in full screen
-  NSToolbarFullScreenAnimationDuration: '-float 0',
-
-  # scrolling column views
-  NSBrowserColumnAnimationSpeedMultiplier: '-float 0'
-}.freeze
-
-# TODO: app defaults
-# showing the Dock
-# defaults write com.apple.dock autohide-time-modifier -float 0
-# defaults write com.apple.dock autohide-delay -float 0
-
-# showing and hiding Mission Control, command+numbers
-# defaults write com.apple.dock expose-animation-duration -float 0
-
-# showing and hiding Launchpad
-# defaults write com.apple.dock springboard-show-duration -float 0
-# defaults write com.apple.dock springboard-hide-duration -float 0
-
-# changing pages in Launchpad
-# defaults write com.apple.dock springboard-page-duration -float 0
-
-# at least AnimateInfoPanes
-# defaults write com.apple.finder DisableAllAnimations -bool true
-
-# sending messages and opening windows for replies
-# defaults write com.apple.Mail DisableSendAnimations -bool true
-# defaults write com.apple.Mail DisableReplyAnimations -bool true
-
-MACOS_DEFAULTS.each_pair do |opt, val|
-  system "defaults write -g #{opt} #{val}"
-end
-
 requested_luarocks_plugins = <<-HEREDOC.gsub(/;.*$/, '').strip.split(/\s+/)
   fennel
 HEREDOC
