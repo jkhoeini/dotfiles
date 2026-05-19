89f518411b282b7f9a17606e815caf2f0e104560
Jonathan Khoeini <mskco.tp@gmail.com>
2026-05-19 17:50:56 +0200

    feat: set the backup extension for home-manager. and add the lang

--- nix/modules/home/default.nix
+++ nix/modules/home/default.nix
@@ -1,24 +1,14 @@
 { username, ... }:
 {
+  home-manager.backupFileExtension = "hm-backup";
+
   home-manager.users.${username} = {
     home.username = username;
     home.homeDirectory = /Users/${username};
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
     home.stateVersion = "24.05";
-
-    # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
-
     home.preferXdgDirectories = true;
-
+    home.language.base = "en_US.UTF-8";
     manual.html.enable = true;
   };
 }
