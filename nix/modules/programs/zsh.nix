dbc5818cbd1f3d3b74e5ea810d772ace0f7d96b4
Jonathan Khoeini <mskco.tp@gmail.com>
2026-05-19 17:50:56 +0200

    fix: remove useless configs from nix zsh

--- nix/modules/programs/zsh.nix
+++ nix/modules/programs/zsh.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 {
-  # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
-
-  # Register Nix zsh in /etc/shells so it can be used as a login shell.
+  programs.zsh.enableGlobalCompInit = false;
   environment.shells = [ pkgs.zsh ];
-  programs.zsh.histFile = "$HOME/.histfile";
-  programs.zsh.histSize = 10000;
-  programs.zsh.enableFzfGit = true;
-  programs.zsh.enableFzfHistory = true;
-  programs.zsh.enableSyntaxHighlighting = true;
 }
